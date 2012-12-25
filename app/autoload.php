ff0e96ae7384157086bb26751e9113db9d59b66f
Janimatti Ellonen <janimatti.ellonen@soprano.fi>
2012-12-25 20:37:25 +0200

    removed references to mongodb

--- app/autoload.php
+++ app/autoload.php
@@ -1,7 +1,6 @@
 <?php
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
-use Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
 
 $loader = require __DIR__.'/../vendor/autoload.php';
 
@@ -13,7 +12,5 @@ if (!function_exists('intl_get_error_code')) {
 }
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
-
-AnnotationDriver::registerAnnotationClasses();
 
 return $loader;
